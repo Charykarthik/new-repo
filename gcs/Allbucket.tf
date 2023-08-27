79a321d56931df1ff54ea5b15fe19de8b0d8d989
Charykarthik <128477106+Charykarthik@users.noreply.github.com>
2023-08-27 09:44:57 +0530

    Add files via upload

---
+++ gcs/Allbucket.tf
@@ -0,0 +1,20 @@
+resource "google_storage_bucket"{
+    name = "sceg_bucket"
+    project = "modernization"
+    location = "us-central1"
+    force_destroy = true
+    storage_class = "standard"
+
+    iam_configuration{
+        public_access_prevention = "enforced"   # "unspecified" or "enforced"
+        uniform_bucket_level_access = true      # uniform level true or false
+        bucket_policy_only = true               # fine-grained true or false
+    }
+    versioning {
+        enabled = true # true or false
+    }
+
+    encryption {
+        default_kms_key_name = "google"
+    }
+}
